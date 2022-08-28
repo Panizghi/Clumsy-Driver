779f4f6bc5681d5ea56744bb74ec802386bf2553
Panizghi <90856064+Panizghi@users.noreply.github.com>
2022-08-28 01:03:10 -0400

    file added

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+   [SerializeField] float turnSpeed = 0.001f;
+   [SerializeField] float moveSpeed = 0.001f; 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +16,11 @@ public class Driver : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   float turnAmount = Input.GetAxis("Horizontal") * turnSpeed; //control with keyboard
+        
+        //object get updated rotation in every signle frame f is for float
+        transform.Rotate(0,0,-turnAmount); //use -ve to rotate in correct direction
+        transform.Translate(0,moveSpeed,0);
         
     }
 }
