25077d2068e2e7b4fd47584d2a5fdf4c1d8c3597
Panizghi <90856064+Panizghi@users.noreply.github.com>
2022-08-31 11:48:45 -0400

    syntax updates

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
-    [SerializedField] float  destroyPack = 0.5f;
+    [SerializeField] float driverDelay = 0.5f; 
+    
+    
+
+    
+
     void OnCollisionEnter2D(Collision2D other) {
         //collsion event details
         Debug.Log("Oppsie!");
@@ -14,12 +19,13 @@ public class Delivery : MonoBehaviour
     // for package pickup 
     //use tags for the package in unity 
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Package" && !hasPackage){ //one delivery at a time
+        if(other.tag == "Package" && !hasPackage){ //pick up the package if driver does not have any 
             Debug.Log("Package Picked up!");
-            hasPackage =true ;
-            Destroy(other.GameObject,destroyPack);
+            hasPackage =true ; //change the flag of of having package 
+            Destroy(other.gameObject, driverDelay);
+           
         }
-        if(other.tag =="Customer" && hasPackage){
+        if(other.tag =="Customer" && hasPackage){ //only deliver to customer when you have the package 
             Debug.Log("Package Delivered");
             hasPackage =false ; //prevent multiple delivery
         }
